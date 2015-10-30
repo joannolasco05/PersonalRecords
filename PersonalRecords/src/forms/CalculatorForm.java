d2bb222002761f05198ef3e73701562c13a2739b
Joan Nolasco <Joan Nolasco@71V9SF1.corp.codetel.com.do>
2015-10-30 08:53:35 -0400

    [NEW] Creating Calculator Structure.

---
+++ PersonalRecords/src/forms/CalculatorForm.java
@@ -0,0 +1,5 @@
+package forms;
+
+public class CalculatorForm {
+	//TODO: Desing the calc.
+}
---
+++ PersonalRecords/src/objects/Calculator.java
@@ -0,0 +1,6 @@
+package objects;
+
+public class Calculator {
+	//TODO: Implements Calc. Operations.
+	
+}
