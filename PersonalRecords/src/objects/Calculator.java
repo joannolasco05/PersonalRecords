455ff86db6772dd949de2286adef644ef82a730a
Joan Nolasco <Joan Nolasco@71V9SF1.corp.codetel.com.do>
2015-10-30 09:29:13 -0400

    [IMP]Finish operation implementation.

--- PersonalRecords/src/objects/Calculator.java
+++ PersonalRecords/src/objects/Calculator.java
@@ -1,11 +1,18 @@
 package objects;
 
 public class Calculator {
-	public int addNumbers(int a, int b){
+	public static int addNumbers(int a, int b){
 		return a + b;
 	}
 	
-	public int subsNumbers(int a, int b){
+	public static int subsNumbers(int a, int b){
 		return a - b;
+	}
+	public static Integer divNumbers(int a, int b){
+		return (b!=0) ? a/b : null ;
+	}
+	
+	public static int multNumbers(int a, int b){
+		return 0;
 	}
 }
