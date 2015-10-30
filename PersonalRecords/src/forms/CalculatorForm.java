ec4393b48953c72d47ba00e9df687472634fd880
Joan Nolasco <Joan Nolasco@71V9SF1.corp.codetel.com.do>
2015-10-30 09:38:17 -0400

    [NEW] Creating Form

--- PersonalRecords/src/forms/CalculatorForm.java
+++ PersonalRecords/src/forms/CalculatorForm.java
@@ -1,5 +1,16 @@
 package forms;
 
-public class CalculatorForm {
-	//TODO: Desing the calc.
+import javax.swing.JFrame;
+
+public class CalculatorForm extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CalculatorForm(){
+		setTitle("Calculator");
+		setBounds(30, 30, 300, 300);
+	}
+	
 }
